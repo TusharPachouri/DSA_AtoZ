e02a139c5ee4d8c173c603f58de515cd29ece145
Tushar Pachouri <tusharpachouri001@gmail.com>
2024-06-07 11:21:35 +0530

    feat: Add classes for Human and update main function

---
+++ AtoZ/OOPs/accessModifiers.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Human {
+    public:
+        string Name;
+        int Age;
+
+        Human(string name, int age){
+            Name = name;
+            Age = age;
+        }
+
+        void introduction() {
+            cout << "NAME: " << Name << endl;
+            cout << "Age: " << Age << endl;
+        }
+};
+
+int main() {
+    Human tushar = Human("Tushar PAchouri", 21);
+    tushar.introduction();
+    // tushar.Name = "Tushar Pachouri";
+    // tushar.Age = 21;
+    // cout << "Name is " << tushar.Name << " and Age is " << tushar.Age << endl;
+    // tushar.introduction();
+    Human superman = Human("Superman1", 50 );
+    superman.introduction();
+    // superman.constructor("superman",89);
+    return 0;
+}
+
